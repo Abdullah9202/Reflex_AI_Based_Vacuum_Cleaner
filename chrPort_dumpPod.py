89ee0b7e76845212d9643256c82b3958910e4b5d
abdullah9202 <abdullahkhurram9202@gmail.com>
2023-12-25 13:15:10 +0500

    thread created

--- chrPort_dumpPod.py
+++ chrPort_dumpPod.py
@@ -1,24 +1,31 @@
 import tkinter as tk
 from tkinter import ttk
+import threading
 
 # Class to create and manage the charging bar
-class ChPort():
-    def chrPortProgressBar():
+class ChBar():
+    def chrPortProgressBar(self):
         # Main window
-        frame = tk.Tk(screenName="Charging Port")
+        self.frame = tk.Tk(screenName="Charging Progress bar")
         # Title
-        frame.title(string="Battery")
+        self.frame.title(string="Battery")
         # Getting the monitor info
-        screen_height = frame.winfo_screenheight()
-        screen_width = frame.winfo_screenwidth()
+        screen_height = self.frame.winfo_screenheight()
+        screen_width = self.frame.winfo_screenwidth()
         # Setting the Windows dimensions
         window_height = int(screen_height * 0.2)
         window_width = int(screen_width * 0.8)
         # Placing the frame
-        frame.geometry(f"220x50+{window_width}+{window_height}")
+        self.frame.geometry(f"220x50+{window_width}+{window_height}")
         # Progress bar
-        progress_Bar = ttk.Progressbar(master=frame, mode="determinate", length=160, orient="horizontal", variable="IntVar")
+        progress_Bar = ttk.Progressbar(master=self.frame, mode="determinate", length=160, orient="horizontal", variable="IntVar")
         # Placing the progress bar on frame
         progress_Bar.pack(pady=15)
 
-        frame.mainloop()
+        self.frame.mainloop()
+        
+    # Function to start threading for Progress bar
+    def startThread(self):
+        # Creating a separate thread for tkinter
+        thread = threading.Thread(target=self.chrPortProgressBar())
+        thread.start()
