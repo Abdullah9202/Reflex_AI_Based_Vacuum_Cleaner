34e0e32d1a24b542ca568f5fc8e4c7b89f35345b
abdullah9202 <abdullahkhurram9202@gmail.com>
2023-12-25 07:17:41 +0500

    Minor Fixes

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ Frame.title("Intelligent Vacuum Cleaner")
 # Screen dimensions
 Frame.setup(width=800, height=800)
 # Creating an object of the class
-Obj1 = vCleaner(9)
+Obj1 = vCleaner()
 # Creating a charging port
 Obj1.dumpingPod()
 # Creating a charging port
@@ -16,7 +16,7 @@ Obj1.chargingPort()
 # Calling dirt function
 Obj1.createDirt(20)
 # Creating vacuum cleaner
-Obj1.vacuumCleaner()
+Obj1.vacuumCleaner(9)
 # Calling start cleaning function
 Obj1.startCleaning()
 
